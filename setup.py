a7c25a73bd1c84d11120ebbac32fbcaeebc2a468
Ben Zimmer <ben.d.zimmer@gmail.com>
2020-02-29 09:36:56 -0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="symbols",
+    version="0.1",
+    author="Ben Zimmer",
+    author_email="nobody@nowhere.com",
+    url="https://github.com/bdzimmer/symbols",
+    packages=["symbols"]
+)
