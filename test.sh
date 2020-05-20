8c67f6a68e3a09b6bd8717a09801a3080c471e18
Ben Zimmer <ben.d.zimmer@gmail.com>
2020-05-20 13:22:22 -0500

    update test script

--- test.sh
+++ test.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-pytest --cov-report term-missing --cov=symbols symbols
+
+# pytest --cov-report term-missing --cov=symbols symbols
+
+pytest --cov-report html --cov=symbols symbols
+# xdg-open htmlcov/index.html
