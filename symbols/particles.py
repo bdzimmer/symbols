e2491ec11dbd987b4d71bc24e3c8973f3c9a1dd3
Ben Zimmer <ben.d.zimmer@gmail.com>
2020-07-25 16:26:38 -0500

    particle texture pregeneration

--- symbols/particles.py
+++ symbols/particles.py
@@ -11,9 +11,10 @@ Particle emitters and related functionality.
 import random
 from typing import List, Tuple, Callable
 
+import cv2
 import numpy as np
 
-from symbols import symbols
+from symbols import symbols, trim
 
 
 # type aliases
@@ -54,3 +55,84 @@ def build_line_emitter(lines: List[symbols.Line], weights) -> Callable[[], Point
         return symbols.interp(line.start, line.end, random.random())
 
     return emit
+
+
+def default_glow_texture(color: Tuple, line_width: int, blur_size: int, glow_strength: float) -> np.ndarray:
+    """create a gaussian-blurred point to be later composited"""
+
+    # original blur_size was 31
+    # original glow_strength = 2.0
+
+    # determine size necessary and location
+    half_width = line_width + blur_size // 2
+    width = half_width * 2 + 1  # add another 2??
+    texture = np.zeros((width, width, 4), dtype=np.uint8)
+
+    pt_0 = (half_width, half_width)  # add 1?
+    pt_1 = pt_0
+
+    if blur_size > 0:
+        # set up for alpha blending
+        # Fill entire region that will be blurred using a fat line
+        # with solid RGB values and alpha 0.
+        # The blur will then blur the alpha of the color across the region.
+        color_mod = (color[0], color[1], color[2], 0)
+        cv2.line(
+            texture,
+            pt_0,
+            pt_1,
+            color_mod,
+            line_width + blur_size // 2,
+            # cv2.LINE_AA  # antialising probably pollutes colors
+        )
+
+    cv2.line(
+        texture,
+        pt_0,
+        pt_1,
+        color,
+        line_width,
+        # cv2.LINE_AA  # antialising probably pollutes colors
+    )
+
+    if blur_size > 0:
+        blurred = cv2.GaussianBlur(
+            texture, (blur_size, blur_size), 0)
+        # blurred = src_chunk
+
+        if blurred is not None:
+            # use only the blur
+            # texture = blurred
+
+            # ensure that all pixels of nonzero alpha have the RGB values of color
+            # this could be done instead of the fat line above
+            # blurred[:, :, 0:3][blurred[:, :, 3] > 0] = (color[0], color[1], color[2])
+
+            # increase intensity of blur and add to original, then clip
+            texture = np.clip(blurred * glow_strength + texture, 0.0, 255.0)
+
+            # max blur with original
+            # texture = np.maximum(blurred, src_chunk)
+
+    return texture
+
+
+def draw_texture(
+        canvas: np.ndarray,
+        pt_xy: Tuple[int, int],
+        texture: np.ndarray,
+        comp_func: Callable):
+    """draw a particle texture onto a canvas with a compositing function"""
+
+    canvas_dim = (canvas.shape[1], canvas.shape[0])
+    texture, (pt_x, pt_y) = trim.trim(texture, pt_xy, canvas_dim)
+
+    # print(
+    #     pt_xy,
+    #     (pt_x, pt_y),
+    #     texture.shape, "->",
+    #     pt_y, "-", (pt_y + texture.shape[0]), ",",
+    #     pt_x, "-", (pt_x + texture.shape[1]))
+
+    canvas_chunk = canvas[pt_y:(pt_y + texture.shape[0]), pt_x:(pt_x + texture.shape[1]), :]
+    canvas[pt_y:(pt_y + texture.shape[0]), pt_x:(pt_x + texture.shape[1]), :] = comp_func(texture, canvas_chunk)
